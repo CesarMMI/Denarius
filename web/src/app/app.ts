a30eb6ef9927feea03d82802167f4d43b4349474
cesar <cesargustavo53@gmail.com>
2025-08-02 18:13:02 -0300

    add angular material

--- web/src/app/app.ts
+++ web/src/app/app.ts
@@ -1,12 +1,10 @@
-import { Component, signal } from '@angular/core';
+import { Component } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
-  styleUrl: './app.scss'
+  imports: [RouterOutlet, MatButtonModule],
+  template: '<router-outlet />',
 })
-export class App {
-  protected readonly title = signal('denarius');
-}
+export class App {}
