a2b9b5bcf9514872355623ea8ba4d8a5068ef3a4
cesar <cesargustavo53@gmail.com>
2026-07-06 20:43:12 -0300

    feat(front): implement menu module

---
+++ front/src/app/menu/components/menu.spec.ts
@@ -0,0 +1,25 @@
+import { provideHttpClient } from '@angular/common/http';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideRouter } from '@angular/router';
+
+import { Menu } from './menu';
+
+describe('Menu', () => {
+	let component: Menu;
+	let fixture: ComponentFixture<Menu>;
+
+	beforeEach(async () => {
+		await TestBed.configureTestingModule({
+			imports: [Menu],
+			providers: [provideHttpClient(), provideRouter([])],
+		}).compileComponents();
+
+		fixture = TestBed.createComponent(Menu);
+		component = fixture.componentInstance;
+		await fixture.whenStable();
+	});
+
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
+});
